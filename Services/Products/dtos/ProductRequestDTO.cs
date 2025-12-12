e967814881119c61ca39ffbbd46df07001c46ca1
Windows 10 <hoangdungnhocom@gmail.com>
2025-12-13 01:56:12 +0700

    feat: admin layout

---
+++ Services/Products/dtos/ProductRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor_FE.Services.Products.dtos;
+
+public class ProductRequestDTO
+{
+    public int? SupplierId { get; set; }
+    public int? CategoryId { get; set; }
+    
+    public string ProductName { get; set; } = null!;
+    public string Barcode { get; set; }
+    public decimal Price { get; set; }
+    public string Unit { get; set; }
+    public string? ProductImg { get; set; }
+}
