137f1f27fbed2d6f6aa36469ea85578b43bc45b0
Quangdung090 <ttramquangdung@gmail.com>
2025-12-13 23:17:06 +0700

    feat: register

---
+++ Models/Auth/RegisterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_FE.Models.Auth
+{
+    public class RegisterRequest
+    {
+        public string username { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
