fc9ed08c74d33697f5d0033b8e3bf79775fb0918
Quangdung090 <ttramquangdung@gmail.com>
2025-12-15 00:36:34 +0700

    feat: update account information

---
+++ Services/Customers/CustomerService.cs
@@ -0,0 +1,57 @@
+﻿using Blazor_FE.Models;
+using Blazor_FE.Services.Base;
+using Blazor_FE.Services.Customers.dtos;
+
+namespace Blazor_FE.Services.Customers
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CustomerService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<APIResponse<CustomerModel>> GetCustomerByAccountIdAsync(int accountId)
+        {
+            try
+            {
+                var url = $"api/v1/Customer/user/{accountId}";
+
+                var response = await _httpClient.GetFromJsonAsync<APIResponse<CustomerModel>>(url);
+                Console.WriteLine(response);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<APIResponse<CustomerModel>> UpdateCustomerAsync(int customerId, CustomerRequestDTO requestDto)
+        {
+            try
+            {
+                var url = $"api/v1/Customer/{customerId}";
+                var response = await _httpClient.PutAsJsonAsync(url, requestDto);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new APIResponse<CustomerModel>
+                    {
+                        StatusCode = (int)response.StatusCode,
+                        Message = "Failed to update customer",
+                        Content = null
+                    };
+                }
+
+                var result = await response.Content.ReadFromJsonAsync<APIResponse<CustomerModel>>();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Services/Customers/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using Blazor_FE.Models;
+using Blazor_FE.Models.Dtos;
+using Blazor_FE.Services.Base;
+using Blazor_FE.Services.Customers.dtos;
+
+namespace Blazor_FE.Services.Customers
+{
+    public interface ICustomerService
+    {
+        public Task<APIResponse<CustomerModel>> GetCustomerByAccountIdAsync(int accountId);
+        public Task<APIResponse<CustomerModel>> UpdateCustomerAsync(int customerId, CustomerRequestDTO requestDto);
+    }
+}
