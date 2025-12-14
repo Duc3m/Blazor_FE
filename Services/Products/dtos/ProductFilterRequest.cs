f69ef09fa72b0435e53aefeffae7c692d870bb05
Duc3m <ducemtran44@gmail.com>
2025-12-14 15:21:14 +0700

    feat: product search

---
+++ Services/Products/dtos/ProductFilterRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Blazor_FE.Services.Products.dtos;
+
+public class ProductFilterRequest
+{
+    public string? SearchTerm { get; set; } = "";
+
+    public List<int>? CategoryIds { get; set; }
+
+    public decimal? MinPrice { get; set; }
+
+    public decimal? MaxPrice { get; set; }
+
+    public string? SortBy { get; set; } = "CreatedAt";
+    public bool SortDescending { get; set; } = false;
+}
